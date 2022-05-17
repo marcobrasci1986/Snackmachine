ee80ccb197983fca27e3d60f09804dc3012236f9
marco.brasci@telenetgroup.be <marco.brasci@telenetgroup.be>
2022-05-17 14:47:17 +0200

    Add mvn wrapper

--- api/src/main/java/be/avidoo/ddd/controller/AppController.java
+++ api/src/main/java/be/avidoo/ddd/controller/AppController.java
@@ -1,6 +1,5 @@
 package be.avidoo.ddd.controller;
 
-import be.avidoo.ddd.core.Person;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class AppController {
 
     @GetMapping("/")
-    public ResponseEntity<Person> index() {
-        Person person = new Person("test");
-        return ResponseEntity.ok(person);
+    public ResponseEntity<String> index() {
+        return ResponseEntity.ok("Snackmachine online");
     }
 }
