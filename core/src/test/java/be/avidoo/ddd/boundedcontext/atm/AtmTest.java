092d3fbbb840b306500a761d1b16421df938dd8a
marco.brasci@telenetgroup.be <marco.brasci@telenetgroup.be>
2022-05-25 14:19:00 +0200

    Chapter 6: Adding Atm bounded context
    Types of Physical Isolation

--- core/src/test/java/be/avidoo/ddd/boundedcontext/atm/AtmTest.java
+++ core/src/test/java/be/avidoo/ddd/boundedcontext/atm/AtmTest.java
@@ -1,18 +1,23 @@
 package be.avidoo.ddd.boundedcontext.atm;
 
+import be.avidoo.ddd.sharedkernel.Money;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class AtmTest {
 
-//    @Test
-//    void takeMoneyWithCommission() {
-//        Atm atm = new Atm();
-//        atm.loadMoney(Money.ONE_EURO);
-//
-//        atm.takeMoney(1D);
-//
-//        assertThat(atm.getMoneyInside().amount()).isEqualTo(0);
-//        assertThat(atm.getMoneyCharged()).isEqualTo(1.01);
-//
-//        // Why cant I access domainEvents?
-//        System.out.println(atm.domainEvents());
-//    }
+    @Test
+    void takeMoneyWithCommission() {
+        Atm atm = new Atm();
+        atm.loadMoney(Money.ONE_EURO);
+
+        atm.takeMoney(1D);
+
+        assertThat(atm.getMoneyInside().amount()).isEqualTo(0);
+        assertThat(atm.getMoneyCharged()).isEqualTo(1.01);
+
+        // @TODO Why cant I access domainEvents?
+        //System.out.println(atm.domainEvents());
+    }
 }
